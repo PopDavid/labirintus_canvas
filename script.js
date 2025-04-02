3edb75a31362f7c275b12514ae075d76c2a91aa6
Pop Dávid Tibor <pop.david.tibor@students.jedlik.eu>
2025-04-02 13:06:28 +0200

    bfs megírása

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Maze {
         // console.log(this.maze)
     }
     Generate_maze(width, height){
+        console.log('rajzol')
         let vis = [];
         let maze = [];
         let queue = [];
@@ -44,11 +45,11 @@ class Maze {
                 }
             }
             catch {
-                console.log(curr, i, queue)
+                // console.log(curr, i, queue)
                 break;
             }
             
-
+            
             
             let neighbours = 0;
             if(curr[0]+1 < width-1 && vis[curr[0]+1][curr[1]]){
@@ -62,21 +63,7 @@ class Maze {
             }
             if(curr[1]-1 > 0 && vis[curr[0]][curr[1]-1]){
                 neighbours++;
-            }
-            
-            /*if(curr[0]+1 < width-1 && curr[1]+1 < height-1 && vis[curr[0]+1][curr[1]+1]){
-                neighbours++;
-            }
-            if(curr[0]+1 < width-1 && curr[1]-1 > 0 && vis[curr[0]+1][curr[1]-1]){
-                neighbours++;
-            }
-            if(curr[0]-1 > 0 && curr[1]+1 < height-1 && vis[curr[0]-1][curr[0]+1]){
-                neighbours++;
-            }
-            if(curr[0]-1 > 0 && curr[1]-1 < height-1 && vis[curr[0]-1][curr[1]-1]){
-                neighbours++;
-            }*/
-                           
+            }               
             if(neighbours <= 1){
                 vis[curr[0]][curr[1]] = true;
                 maze[curr[0]][curr[1]] = true;
@@ -92,7 +79,7 @@ class Maze {
                 if(curr[1]-1 > 0){
                     queue.push([curr[0], curr[1]-1]);
                 }
-    
+                
                 if(curr[0] == width-2){
                     
                     lastcell = curr;
@@ -118,13 +105,81 @@ class Maze {
             // else rem++
         }
     }
+
+    Give_help(length){
+        let queue = [];
+        let vis = [];
+        let parents = [];
+        for(let i = 0;i<n;i++){
+            let temp = [];
+            let temp2 = [];
+            for(let j = 0;j<n;j++){
+                temp.push(false);
+                temp2.push({x: -1, y: -1});
+            }
+            vis.push(temp);
+            parents.push(temp2);
+        }
+        console.log(vis)
+        parents[player.y][player.x] = {x: player.y, y: player.x};
+
+        queue.push([player.y, player.x]);
+        vis[player.y][player.x] = true;
+        while(queue.length){
+            let curr = queue[0];
+            let x = curr[0];
+            let y = curr[1];
+            queue.splice(0, 1);
+            if(this.maze[x+1][y] && !vis[x+1][y]){
+                queue.push([x+1, y]);
+                parents[x+1][y] = {x: x, y: y};
+                vis[x+1][y] = true;
+            }
+            if(this.maze[x-1][y] && !vis[x-1][y]){
+                queue.push([x-1, y]);
+                parents[x-1][y] = {x: x, y: y};
+                vis[x-1][y] = true;
+            }
+            if(this.maze[x][y+1] && !vis[x][y+1]){
+                queue.push([x, y+1]);
+                parents[x][y+1] = {x: x, y: y};
+                vis[x][y+1] = true;
+            }
+            if(this.maze[x][y-1] && !vis[x][y-1]){
+                queue.push([x, y-1]);
+                parents[x][y-1] = {x: x, y: y};
+                vis[x][y-1] = true;
+            }
+        }
+        let ans_array = []
+        let ans = {x: apple.y,y: apple.x};
+        try {
+            console.log(parents[ans.x][ans.y].x != player.x + " " + parents[ans.x][ans.y].y != player.y);
+        }catch(e){
+            console.log(parents, ans);
+        }
+        while((parents[ans.x][ans.y].x != player.y || parents[ans.x][ans.y].y != player.x)){
+            ans = parents[ans.x][ans.y];
+            ans_array.push(ans);
+            // try {
+            //     console.log(parents[ans.x][ans.y].x != player.x + " " + parents[ans.x][ans.y].y != player.y);
+            // }catch(e){
+            //     console.log(parents, ans);
+            // }
+        }
+        ans_array = ans_array.reverse();
+        console.log(parents[ans.x][ans.y].x != player.x, parents[ans.x][ans.y].y != player.y)
+        console.log(ans_array)
+        let tartok = 0;
+        while(tartok < length) {
+            ans = ans_array[tartok]
+            console.log(tartok, length)
+            drawTile(ans.x, ans.y, false, x)
+            tartok++;
+        }
+    }
 }
-let n = 30; 
-let maze = new Maze(n)
-let score = 0;
-let time_started = 0;
-let can_win = 0;
-let tileSize = c.width/n;
+
 class Player {
     constructor () {
         this.x = 1;
@@ -134,31 +189,40 @@ class Player {
         this.move_counter = 0
     }
 }
+let n = 100; 
+let maze = new Maze(n)
+let score = 0;
+let time_started = 0;
+let can_win = 0;
+let tileSize = c.width/n;
 let player = new Player
 
 
 function change_size(Myn) {
+    console.log('change size');
     n = Myn;
-    console.log(n)
     maze = new Maze(n)
     score = 0;
     time_started = 0;
     can_win = 0;
     tileSize = c.width/n;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMaze();
-    drawPlayer();
-    draw_apple();
-    stopTimer()
+    drawMaze(n, true);
+    stopTimer();
     score = 0;
     document.getElementById('score').innerText = score;
     elapsedTime = 0; 
     document.getElementById('timer').innerText = elapsedTime;
     time_started = 0
+    player.x = 1
+    player.y = 1
     do {
         apple.x = Math.floor(Math.random() * n);
         apple.y = Math.floor(Math.random() * n);
     } while (maze.maze[apple.y][apple.x] == false || (apple.x === player.x && apple.y === player.y));
+    draw_apple();
+    console.log(apple.x ,apple.y, player.x, player.y)
+    drawPlayer();
 }
 
 const apple_pic = document.getElementById('apple');
@@ -166,20 +230,21 @@ cat1 = document.getElementById('cat1');
 cat2 = document.getElementById('cat2');
 portal = document.getElementById('portal');
 bush = document.getElementById('bush');
+x = document.getElementById('x');
 let apple = {
     x: 0,
     y: -10
 }
 window.onload = () => {
-    drawMaze();
+    drawMaze(n, true);
     drawPlayer();
-    draw_apple();
     do {
         apple.x = Math.floor(Math.random() * n);
         apple.y = Math.floor(Math.random() * n);
     } while (maze.maze[apple.y][apple.x] == false || (apple.x === player.x && apple.y === player.y));
+    draw_apple()
+    // console.log(apple.x, apple.y)
 }
-
 
 function startTimer() {
     startTime = Date.now();
@@ -206,25 +271,50 @@ function reachApple ()  {
 }
 
 function drawPlayer () {
-    // ctx.scale(flip ? 0 : 1, 180);
-
-    if (player.oriant === 'left') {
-        ctx.drawImage(player.img, player.x * tileSize, player.y * tileSize, tileSize, tileSize);     
-    }
-    else {
-        ctx.drawImage(player.img, player.x * tileSize, player.y * tileSize, tileSize, tileSize);     
-    }
+    if (player.oriant === 'left') ctx.drawImage(player.img, player.x * tileSize, player.y * tileSize, tileSize, tileSize);     
+    else ctx.drawImage(player.img, player.x * tileSize, player.y * tileSize, tileSize, tileSize);     
     player.img = player.img === cat1 ? cat2 : cat1;
 }
 
-function drawMaze() {
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (maze.maze[i][j] == false) {
-                ctx.drawImage(bush, j * tileSize, i * tileSize, tileSize, tileSize);
+function drawTile(x, y, wait, img = bush) {
+    setTimeout(() => {
+        ctx.drawImage(img, y * tileSize, x * tileSize, tileSize, tileSize);
+    }, 20 + wait*50);
+}
+
+
+function drawMaze(num = n, firstdraw = false) {
+    if(firstdraw){
+
+        if(num == 1){
+            drawTile(0, 0)
+            return 0
+        }
+        drawMaze(num-1, true);
+        
+        for (let i = 0; i < num; i++) {
+            try {
+                if (maze.maze[i][num-1] == false) {
+                    drawTile(i, num-1, num);
+                }
+                if (maze.maze[num-1][i] == false) {
+                    drawTile(num-1, i, num);
+                }
+            }
+            catch (e) {
+                console.log(num, i)
+            }
+        }
+    }else{
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (maze.maze[i][j] == false) {
+                    ctx.drawImage(bush, j * tileSize, i * tileSize, tileSize, tileSize);
+                }
             }
         }
     }
+    
     if (can_win) {
        ctx.drawImage(portal, tileSize, tileSize, tileSize, tileSize);
     }
@@ -240,6 +330,10 @@ window.addEventListener('keydown', (e) => {
         case 'w':
         case 'ArrowUp':
             if (maze.maze[player.y - 1][player.x] == true) {
+                if (!time_started) {
+                    startTimer();
+                    time_started = 1;
+                }
                 player.y -= 1;
                 player.move_counter++;
                 document.getElementById('move_counter').innerText = player.move_counter
@@ -248,6 +342,10 @@ window.addEventListener('keydown', (e) => {
         case 's':
         case 'ArrowDown':
             if (maze.maze[player.y + 1][player.x] == true) {
+                if (!time_started) {
+                    startTimer();
+                    time_started = 1;
+                }
                 player.y += 1;
                 player.move_counter++;
                 document.getElementById('move_counter').innerText = player.move_counter
@@ -256,6 +354,10 @@ window.addEventListener('keydown', (e) => {
         case 'a':
         case 'ArrowLeft':
             if (maze.maze[player.y][player.x - 1] == true ) {
+                if (!time_started) {
+                    startTimer();
+                    time_started = 1;
+                }
                 player.x -= 1;
                 player.move_counter++;
                 document.getElementById('move_counter').innerText = player.move_counter
@@ -264,12 +366,11 @@ window.addEventListener('keydown', (e) => {
             break;
         case 'd':
         case 'ArrowRight':
-            if (!time_started) {
-                startTimer();
-                time_started = 1;
-            }
-            
             if (maze.maze[player.y][player.x + 1] == true ) {
+                if (!time_started) {
+                    startTimer();
+                    time_started = 1;
+                }
                 player.x += 1;
                 player.move_counter++;
                 document.getElementById('move_counter').innerText = player.move_counter
@@ -288,9 +389,11 @@ window.addEventListener('keydown', (e) => {
     drawMaze();
     draw_apple();
     drawPlayer();
-});
+    // e.preventDefault();
+}, true);
 
 document.getElementById('easy_BTN').addEventListener('click', () => change_size(50))
 document.getElementById('medium_BTN').addEventListener('click', () => change_size(70))
 document.getElementById('hard_BTN').addEventListener('click', () => change_size(100))
 document.getElementById('custom_BTN').addEventListener('click', (e) => change_size(document.getElementById('input1').value), true)
+document.getElementById('help').addEventListener('click', () => {maze.Give_help(100000)})
